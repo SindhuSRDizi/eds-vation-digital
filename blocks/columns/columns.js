b8ddde088a229e8e2ad5b2de117f0831aa7116a5
SindhuSRDizi <sindhu.sr@dizitalpods.com>
2025-07-17 14:33:41 +0530

    column variation and balck bg card icon alignment fixed

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -2,14 +2,20 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
-  // setup image columns
+  // Add black-bg or white-bg to the block based on its class
+  if (block?.classList.contains('black-column-bg')) {
+    block.classList.add('black-column-bg');
+  } else {
+    block.classList.add('white-column-bg');
+  }
+
+  // Setup image columns
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
       if (pic) {
         const picWrapper = pic.closest('div');
         if (picWrapper && picWrapper.children.length === 1) {
-          // picture is only content in column
           picWrapper.classList.add('columns-img-col');
         }
       }
